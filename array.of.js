f6846dc84eee2d19ab2fdb38cd7cd6fc08a1c68c
GuilhermeSalah <141652554+GuilhermeSalah@users.noreply.github.com>
2023-08-28 11:35:22 -0400

    Add files via upload

---
+++ array.of.js
@@ -0,0 +1,2 @@
+const nums = Array.of(2,4,6,8);
+console.log(nums); // saída: [2,4,6,8]
---
+++ filter.js
@@ -0,0 +1,3 @@
+const arr = [2.4.6.8];
+const novo = arr.filter(num => num > 5);
+console.log(novo); // saída: [6,8]
